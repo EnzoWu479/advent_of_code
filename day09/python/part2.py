09d72e6938cf9c43c60aa98253d58503441043fc
EnzoWu <75583251+EnzoWu479@users.noreply.github.com>
2024-12-11 18:48:36 -0300

    day09 part02

--- day09/python/part2.py
+++ day09/python/part2.py
@@ -1,38 +1,37 @@
-with open('inputtest.txt') as f:
-    for line in f:
-        diskmap = line
+with open('input.txt', 'r') as file:
+    line = []
+    blanks = []
+    blocks = []
+    n = 0
 
-empty = False
-count = 0
-main_blocks = []
+    for i, c in enumerate(file.read()):
+        if i % 2 == 1:
+            line += ['.'] * int(c)
+            blanks.append([int(c), n])
+        else:
+            line += [str(i // 2)] * int(c)
+            blocks.append([int(c), n])
+        n += int(c)
 
-for index, character in enumerate(list(diskmap)[::-1]):
-    main_blocks.append({
-        "index": index,
-        "size": int(character),
-        "value": count if not empty else None
-    })
-    if not empty:
-        count += 1
-    empty = not empty
+r = len(blocks) - 1
+while r > 0:
+    l = 0
+    while all(a < b for a, b in zip(blanks[l], blocks[r])):
+        l += 1
 
-files = list(filter(lambda x : x["value"] != None, main_blocks))
-files.sort(key=lambda x : x["value"], reverse=True)
+    if blanks[l][0] >= blocks[r][0] and blanks[l][1] < blocks[r][1]:
+        for i in range(blocks[r][0]):
+            line[blanks[l][1] + i], line[blocks[r][1] + i] = (
+                line[blocks[r][1] + i],
+                line[blanks[l][1] + i],
+            )
+        blanks[l][0] -= blocks[r][0]
+        blanks[l][1] += blocks[r][0]
 
-soma = 0
-last_num_index = 0
-for i, block in enumerate(main_blocks):
-    is_empty = block["value"] == None
-    if is_empty:
-        biggest_that_fit = None
-        for file in files:
-            if file["size"] <= block["size"]:
-                biggest_that_fit = file
-                break
-        if biggest_that_fit != None:
-            num = main_blocks[numbr_blocks[-last_num_index-1]]
-        last_num_index += 1
-    else:
-        num = block["value"]
-    soma += i * num
-print(soma)
+        if blanks[l][0] == 0:
+            blanks.pop(l)
+
+    r -= 1
+
+result = sum(i * int(c) for i, c in enumerate(line) if c != '.')
+print(result)
